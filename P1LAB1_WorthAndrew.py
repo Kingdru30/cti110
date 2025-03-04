dec5bb7ae0455714fd2c6dcd0bb5b5b4f487cb6c
Kingdru30 <Kingdru30@gmail.com>
2025-03-04 12:26:30 +0300

    Add files via upload
    First submission

---
+++ P1LAB1_WorthAndrew.py
@@ -0,0 +1,13 @@
+# Andrew Worth  
+# 3/2/2025  
+# P1LAB1  
+# A Python program that takes user's first and last names as input and displays a welcome message.
+
+# Prompting user to input their first name
+first_name = input("Enter your first name: ")
+
+# Prompting user to input their last name
+last_name = input("Enter your last name: ")
+
+# Displaying a welcoming message with the user's full name
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")

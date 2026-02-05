75af87d54a9880614ea18a3fa2baa67ee36132a5
Kingdru30 <Kingdru30@gmail.com>
2026-02-05 16:01:24 +0000

    final P1LAB1

--- P1lab1_worth.py
+++ P1lab1_worth.py
@@ -4,6 +4,19 @@
 # 2/5/2026
 # pratice Python program
 
-name = "Big Daddy"
-print ("nice to meet you",name)
+# name = "Big Daddy"
+# greet the user
+
+first_name = input(("Hi, whats your first name?"))
+last_name = input(("Hi, whats your last name?"))
+
+print ("nice to meet you",first_name, last_name)
 print () # extra blank line
+hobby = input("whats type of 'stuff'do you like?")
+print( "I am a Computer but i would probably like ", hobby, "too" )
+
+# example of a loop
+print("i cant repeat myself!")
+count = int(input("how many times should i repeat?"))
+for time in range(count):
+    print("i know a song that drives poeple crazy,")
